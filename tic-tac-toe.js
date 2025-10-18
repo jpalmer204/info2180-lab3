c0d379b6106e21fab23994262e2b7238da7291f2
Jordan Palmer <palmerjordan2k@gmail.com>
2025-10-17 23:04:30 -0500

    Add X and O click

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,8 +1,17 @@
 window.addEventListener('DOMContentLoaded', () => {
   const squares = Array.from(document.querySelectorAll('#board div'));
+  let currentPlayer = 'X';
 
   // Add the "square" CSS class to each one
   squares.forEach(square => {
     square.classList.add('square');
+
+    square.addEventListener('click', () => {
+        if (square.textContent !== '') return;
+        square.textContent = currentPlayer;
+        square.classList.add(currentPlayer);
+
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    });
   });
 });
